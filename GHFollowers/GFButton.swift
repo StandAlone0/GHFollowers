3c12260ad5474fc8f5c73d22d1b77945abaa5118
StandAlone0 <evgeniymoroz1999@mail.ru>
2024-09-11 18:21:40 +0300

    Create Button and TextField

---
+++ GHFollowers/GFButton.swift
@@ -0,0 +1,33 @@
+//
+//  GFButton.swift
+//  GHFollowers
+//
+//  Created by Евгений on 11.09.2024.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        configuration = .tinted()
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
