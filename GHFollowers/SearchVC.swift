ea822132a1eb1384cc2bb8ba417fdc7241478bca
StandAlone0 <evgeniymoroz1999@mail.ru>
2024-09-11 14:49:50 +0300

    Setup basic navigation

---
+++ GHFollowers/SearchVC.swift
@@ -0,0 +1,20 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by Евгений on 11.09.2024.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemRed
+    }
+    
+
+
+}
